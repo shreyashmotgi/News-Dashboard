1991c70cf63d4d60738c82f43c91b9cfcd967abf
shreyashmotgi <shreyashmotagi99@gmail.com>
2026-04-10 20:32:08 +0530

    Implemented loading and improved api error handling

--- src/Articles.jsx
+++ src/Articles.jsx
@@ -2,9 +2,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CardActionArea from "@mui/material/CardActionArea";
-import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import "./Articles.css";
 
--- src/News.jsx
+++ src/News.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Articles from "./Articles";
 import Navbar from "./Navbar";
 import Category from "./Category";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function News() {
   const API_KEY = import.meta.env.VITE_API_KEY || "your_key";
@@ -11,6 +13,7 @@ export default function News() {
   const [query, setQuery] = useState("");
   const [category, setCategory] = useState("general");
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   let updateSearch = (value) => {
     setQuery(value);
@@ -24,6 +27,7 @@ export default function News() {
 
   useEffect(() => {
     const fetcharticles = async () => {
+      setLoading(true);
       try {
         let url;
 
@@ -37,7 +41,10 @@ export default function News() {
         let data = await res.json();
         console.log(data);
 
-        if (data.articles) {
+        if (data.errors) {
+          setError(data.errors[0]);
+          setArticles([]);
+        } else if (data.articles) {
           setArticles(data.articles);
           setError(null);
         } else {
@@ -47,6 +54,7 @@ export default function News() {
       } catch (err) {
         setError("Error fetching data");
       }
+      setLoading(false);
     };
 
     fetcharticles();
@@ -56,8 +64,20 @@ export default function News() {
     <div>
       <Navbar updateSearch={updateSearch} />
       <Category updateCategory={updateCategory} />
-      {error && <h1 style={{ color: "red" }}>{error}</h1>}
-      <Articles articles={articles} />
-    </div>
-  );
+      {error ? (
+        <h1 style={{ color: "red" }}>{error}</h1>
+      ) : loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Articles articles={articles} />
+      )}
+          </div>
+        );
 }
